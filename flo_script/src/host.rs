0d5b52de86e75585bb595d7fcfb3d8f5a444df55
Andrew Hunter <andrew@logicalshift.co.uk>
2019-07-30 22:10:16 +0100

    Add functions for retrieving the editor and notebook from a script host

--- flo_script/src/host.rs
+++ flo_script/src/host.rs
@@ -5,6 +5,20 @@ use super::notebook::*;
 /// Implementations of this trait host a scripting language used with FlowBetween.
 ///
 pub trait FloScriptHost {
-    type Notebook : FloScriptNotebook;
-    type Editor : FloScriptEditor;
+    type Notebook   : FloScriptNotebook;
+    type Editor     : FloScriptEditor;
+
+    ///
+    /// Retrieves the script notebook for this host
+    /// 
+    /// The notebook can be used to attach input streams to input symbols and retrieve output streams from scripts.
+    /// 
+    fn notebook<'a>(&'a self) -> &'a Self::Notebook;
+
+    ///
+    /// Retrieves the editor for this host
+    /// 
+    /// The editor can be used to define input symbols, scripts and namespaces
+    /// 
+    fn editor<'a>(&'a self) -> &'a Self::Editor;
 }
