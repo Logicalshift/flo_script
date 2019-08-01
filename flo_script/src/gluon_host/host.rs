b2828d955e7c53950fcb243c43536ddd15ed329b
Andrew Hunter <andrew@logicalshift.co.uk>
2019-08-01 09:18:44 +0100

    Add a way to receive when the edits for a script have completed

--- flo_script/src/gluon_host/host.rs
+++ flo_script/src/gluon_host/host.rs
@@ -2,9 +2,15 @@ use super::core::*;
 use super::editor::*;
 
 use futures::*;
+use futures::stream;
+use futures::sync::oneshot;
 use desync::{Desync, pipe_in};
 
 use std::sync::*;
+
+lazy_static! {
+    static ref FINISHED_EDITS: Desync<()> = Desync::new(());
+}
 
 ///
 /// A script host for Gluon scripts
@@ -30,9 +36,31 @@ impl GluonScriptHost {
 
     ///
     /// Receives edits from the specified streams
+    /// 
+    /// The returned future indicates when the stream is consumed. It can signal that it's cancelled in the event that the stream is
+    /// not completely consumed before the script host is dropped.
     ///
-    pub fn send_gluon_edits<EditStream: 'static+Send+Stream<Item=GluonScriptEdit, Error=()>>(&self, edits: EditStream) {
+    pub fn send_gluon_edits<EditStream: 'static+Send+Stream<Item=GluonScriptEdit, Error=()>>(&self, edits: EditStream) -> impl Future<Item=(), Error=oneshot::Canceled> {
         let core = Arc::clone(&self.core);
+
+        // Create a future to notify when the stream is finished
+        let (notify_finished, finished) = oneshot::channel();
+
+        // Alter the edit stream so it notifies the future once it's exhausted
+        let mut notify_finished         = Some(notify_finished);
+        let mut edits                   = edits;
+        let edits                       = stream::poll_fn(move || {
+            let edit = edits.poll();
+
+            match edit {
+                Ok(Async::Ready(None)) => {
+                    notify_finished.take().map(|notify_finished| FINISHED_EDITS.desync(move |_| { notify_finished.send(()).ok(); }));
+                    Ok(Async::Ready(None))
+                },
+
+                _ => edit
+            }
+        });
 
         // Pipe the edits straight to the core
         pipe_in(core, edits, |core, edit| {
@@ -40,5 +68,8 @@ impl GluonScriptHost {
                 core.edit(edit);
             }
         });
+
+        // Result is a future indicating when we've exhausted the stream (it'll signal cancelled if we stop polling the stream, which will happen if the host is dropped before it completes)
+        finished
     }
 }
--- flo_script/src/lib.rs
+++ flo_script/src/lib.rs
@@ -1,3 +1,5 @@
+#[macro_use] extern crate lazy_static;
+
 mod symbol;
 mod editor;
 mod notebook;
