0b6fa6a8fff7a6961e162bcbf2afb4deaa0e5cac
Andrew Hunter <andrew@logicalshift.co.uk>
2019-08-01 21:57:50 +0100

    Add the notion of a streaming script and a computed script (one of which receives and produces streams, and the other works more like a cell in a spreadsheet.

--- flo_script/src/editor.rs
+++ flo_script/src/editor.rs
@@ -21,7 +21,23 @@ pub enum ScriptEdit {
     SetInputType(FloScriptSymbol, TypeId),
 
     /// Specifies that a particular symbol is used as a script, and the contents of the script that it should evaluate
-    SetScript(FloScriptSymbol, String),
+    /// 
+    /// This script will be a streaming script. It receives any inputs as streams and produces its output as a stream.
+    /// Streaming scripts will stall when nothing is reading from their output. Only the first script to read from the
+    /// output is guaranteed to receive all of the symbols the script produces.
+    SetStreamingScript(FloScriptSymbol, String),
+
+    /// Specifies that a particular symbol is used as a script, and the contents of the script that it should evaluate
+    /// 
+    /// This script will be a 'computing' script. Inputs are treated as streams of states. When this script reads one,
+    /// the most recent symbol from the input is passed in. The value will be recomputed, producing a stream of output,
+    /// in the event that any of the input values it previously computed change.
+    /// 
+    /// These scripts essentially act like a cell in a spreadsheet, producing a stream of states from a set of input
+    /// values.
+    /// 
+    /// Nothing will be computed until the first value is 'pulled' from the resulting stream.
+    SetComputingScript(FloScriptSymbol, String),
 
     /// Performs one or more edits in a namespace (names declared in this namespace are only visible from scripts that are
     /// also in that namespace)
