ad351f2c9d50619d48dcf746ca1de27fa8fd4723
Andrew Hunter <andrew@logicalshift.co.uk>
2019-08-04 15:22:19 +0100

    Use function-calling variant of our test method here

--- flo_script/tests/gluon.rs
+++ flo_script/tests/gluon.rs
@@ -16,31 +16,33 @@ fn threads_have_same_modules() {
     // Import two modules into them (same name in both threads, but different modules)
     fn module1(thread: &Thread) -> vm::Result<ExternModule> {
         ExternModule::new(thread, record! {
-            module => "Module1".to_string()
+            module => primitive!(1, |_: ()| "Module1".to_string())
         })
     }
 
     fn module2(thread: &Thread) -> vm::Result<ExternModule> {
         ExternModule::new(thread, record! {
-            module => "Module2".to_string()
+            module => primitive!(1, |_: ()| "Module2".to_string())
         })
     }
 
     import::add_extern_module(&thread1, "test_module", module1);
     import::add_extern_module(&thread2, "test_module", module2);
 
-    // Check that test_module returns different things on different threads
+    // Check that test_module returns the same things on different threads
     let mut compiler = Compiler::new();
 
     let module1 = compiler.run_expr::<String>(&thread1, "test", r#"
         let test_module = import! "test_module"
-        test_module.module
+        let s = test_module.module()
+        s
     "#).map_err(|err| err.emit_string(&compiler.code_map())).unwrap();
     assert!(&module1.0 == "Module2");
  
     let module2 = compiler.run_expr::<String>(&thread2, "test", r#"
         let test_module = import! "test_module"
-        test_module.module
+        let s = test_module.module()
+        s
     "#).map_err(|err| err.emit_string(&compiler.code_map())).unwrap();
     assert!(&module2.0 == "Module2");
 }
